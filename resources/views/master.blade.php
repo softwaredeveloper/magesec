cbd38c56248be5fdad5edaf36fbb53fee02d5f8a
Martin Pachol <martin@magemojo.com>
2017-01-03 19:43:55 -0500

    master update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,14 +1,14 @@
-<html>
-    <head>
-        <title>App Name - @yield('title')</title>
-    </head>
-    <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
+<!DOCTYPE html>
+<html lang="en">
 
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <title>Mage Security Council</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/styles.min.css">
+</head>
+<body>
+
+</body>
+
 </html>
