fbbe4585ca8bac7f093fce4bce87bfc31d6e955f
Martin Pachol <martin@magemojo.com>
2017-01-04 16:27:49 -0500

    master update

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
