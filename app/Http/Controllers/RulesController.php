279e69d4a56fccc268f9455df2455cdf48ea70eb
Martin Pachol <martin@magemojo.com>
2017-01-04 16:40:03 -0500

    master update

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -33,7 +33,20 @@ class RulesController extends Controller
 		    } else {
 		      $user_id = 0;
 		    }
-		    DB::insert("insert into malware_rules (name, contributor, created_at, updated at, active, under_review, approved_by, type, rules) values('".mysqli_real_escape_string($request->name)."',".$user_id.",now(),now(),0,1,0,'STANDARD','".mysqli_real_escape_string($request->rule)."')");
+		    #DB::insert("insert into malware_rules (name, contributor, created_at, updated at, active, under_review, approved_by, type, rules)
+		    #values('".mysqli_real_escape_string($request->name)."',".$user_id.",now(),now(),0,1,0,'STANDARD','".mysqli_real_escape_string($request->rule)."')");
+		    DB::insert('insert into malware_rules (name, contributor, created_at, updated_at, active, under_review, approved_by, type, rules) values (:name, :contributor, :created_at, :updated_at, :active, :under_review, :approved_by, :type, :rules)',
+		      [
+		      'name' => $request->name,
+		      'contributor' => $user_id,
+		      'created_at' => 'now()',
+		      'updated at' => 'now()',
+		      'active' => 0
+		      'under_review' => 1
+		      'approved_by' => 0
+		      'type' => 'STANDARD'
+		      'rules' => $request->rule
+		      ]);
 		  }
         }
 
