ac7e20f3dc25186866a80219d6a8877e63ace1c8
Martin Pachol <martin@magemojo.com>
2017-01-04 16:05:22 -0500

    master update

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class RulesController extends Controller
 {
