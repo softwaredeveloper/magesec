899709bc00788e4f354e0b1990ba4eb41de6b5c2
Martin Pachol <martin@magemojo.com>
2017-01-04 15:30:47 -0500

    master update

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -11,11 +11,20 @@ class PostController extends Controller
     public function create(Request $request)
     {
         // Validate and store the rule...
-        $this->validate($request, [
+        $validator = validator::make($request->all(), [
         'name' => 'required|regex:^[A-Za-z][A-Za-z0-9_-]*$|max:20',
         'rule' => 'required',
         ]);
-        return view('scanner-rules');
+
+        if (!$validator->fails()) {
+          file_put_contents('temp/$request->name.'.yar',$rule);
+          $result = exec('yara -r '.temp/$request->name.'.yar' temp/testfile.txt');
+          print_r($result);
+
+        }
+        #return redirect('scanner-rules')
+		#            ->withErrors($validator)
+        #    ->withInput();
     }
 
 
