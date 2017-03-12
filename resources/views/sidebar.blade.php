10210b8d277448d635beb61a6e4994d6cc017fe7
Eric Hileman <eric@magemojo.com>
2017-03-12 19:37:47 -0400

    Misc content updates.

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -5,8 +5,8 @@
                 Magento Security Tools
               </h1>
               <ul class="msc-list">
-                <li><a href="/scanner">Mage Malware Scanner</a></li>
                 <li><a href="http://magereport.com/">Mage Report</a></li>
+                <li><a href="/scanner">Mage Malware Scanner</a></li>
                 <li><a href="https://github.com/magesec/magesecuritypatcher">Mage Security Patcher</a></li>
               </ul>
             </article>
@@ -16,11 +16,18 @@
               </h1>
               <ul class="msc-list">
                 <li><a href="https://magento.com/security">Magento Security Center</a></li>
+                <li><a href="https://github.com/talesh/response">Magento Security Incident Response Plan</a></li>
               </ul>
             </article>
             <article class="msc-block">
               <p>
-                Follow us on twitter <em><a href="https://twitter.com/mage_sec">@mage_sec</a></em> for the latest Magento security news.
-            </article>            
+                <em>Follow us on twitter <a href="https://twitter.com/mage_sec">@mage_sec</a> for the latest Magento security news.</em>
+              </p>
+            </article>
+            <article class="msc-block">
+              <p>
+                <em>Contribute to the magesec.org website on github <a href="https://github.com/magesec/magesec">magesec/magesec</a>.</em>
+              </p>
+            </article>                         
           </div>          
         </div>
