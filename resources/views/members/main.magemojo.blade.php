fedd0458694e3d67eef209be1eebf9e400026504
Martin Pachol <martin@magemojo.com>
2017-03-14 14:23:57 -0400

    members pages additions and randomization

---
+++ resources/views/members/main.magemojo.blade.php
@@ -0,0 +1,5 @@
+		<div class="col-md-6">
+			<a href="https://www.magemojo.com" target="_blank"><img src="images/magemojo-logo.png" alt="magemojo" height="50" /></a>
+			<p>Founded in 2009, MageMojo specializes exclusively in hosting for Magento. MageMojo was the first hosting company to be created exclusively to only host and support the Magento platform. By focusing solely on Magento they are able to provide Merchants with higher Magento performance, deeper Magento support, and lower hosting costs then their competitors.
+			</p>
+		</div>
